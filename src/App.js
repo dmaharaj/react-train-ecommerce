d39e50317c8d4065455226b4b8be4ec2f175408b
dmaharaj <david.maharaj@gmail.com>
2020-02-03 13:54:57 -0800

    learning higher order components withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import { Switch, Route, Link, withRouter} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = props => {
+  console.log(props);
+  return(
+  <div>
+    <h1>Hats Page</h1>
+    <div>
+      <button onClick={()=> props.history.push('/topics')}>Topics</button>
+    </div>
+  </div>
+  );
+};
+
+const TopicsList = (props) => {
+  return (
+    <div>
+      <h1>Topic List Page</h1>
+      <Link to={`${props.match.url}/13`}>To Topic 13</Link>
+      <Link to={`${props.match.url}/17`}>To Topic 17</Link>
+    </div>
+  );
+};
+
+const TopicDetails = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic Detail Page { props.match.params.topicId }</h1>
+      <Link to='/topics'>Topics List</Link>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-        <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact={true} path='/hats' component={HatsPage}/>
+        <Route exact={true} path='/topics' component={TopicsList}/>
+        <Route path='/topics/:topicId' component={TopicDetails}/>
+      </Switch>
     </div>
   );
 }
