e1de6b507d96f784558fa5bb9c55338b6c3880e6
dmaharaj <david.maharaj@gmail.com>
2020-02-29 13:56:33 -0800

    adding counter for cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,10 +6,10 @@ import { ReactComponent as ShoppingIcon } from '../../shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -17,5 +17,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = ({ cart: { cartItems }}) => ({
+    itemCount: cartItems.reduce((accumulatedQty, cartItem) => (accumulatedQty + cartItem.quantity), 0)
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
