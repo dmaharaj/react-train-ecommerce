8d4d09fee9bf22a8106c6fcf382c936bce8312af
dmaharaj <david.maharaj@gmail.com>
2020-03-15 11:05:16 -0700

    adding and removing items from cart using arrows in checkout item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -33,4 +33,5 @@ const CartDropdown = ({cartItems, history, dispatch}) => (
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 });
+
 export default withRouter(connect(mapStateToProps)(CartDropdown));
