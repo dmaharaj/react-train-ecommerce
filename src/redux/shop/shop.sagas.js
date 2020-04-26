b4fabc864639278e9e31dc3b2c558cfe37cf215f
dmaharaj <david.maharaj@gmail.com>
2020-04-26 14:47:54 -0700

    sagafying another area of actions

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,12 +1,33 @@
-import { takeEvery } from 'redux-saga/effects';
-
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 import ShopActionTypes from './shop.types';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 
 //at every yield, handing control back to saga middleware to decide how to proceed next
 
 export function* fetchCollectionsAsync() {
     
-    yield console.log('I am fired');
+    yield console.log('I am fired and sagafied');
+
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch(error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
+        
+    // }
+    // dispatch(fetchCollectionsStart()); 
+
+    // collectionRef.get().then(snapshot => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     console.log("collectionsMap: ", collectionsMap);
+
+    //     dispatch(fetchCollectionsSuccess(collectionsMap));
+    // }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
 }
 
 export function* fetchCollectionsStart() {
